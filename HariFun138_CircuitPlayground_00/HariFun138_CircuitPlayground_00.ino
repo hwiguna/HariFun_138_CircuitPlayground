359aac77ac62a57b6310d202473e15bbb6af9236
Hari Wiguna <hwiguna@gmail.com>
2017-05-13 00:02:26 -0500

    sync

--- HariFun138_CircuitPlayground_00/HariFun138_CircuitPlayground_00.ino
+++ HariFun138_CircuitPlayground_00/HariFun138_CircuitPlayground_00.ino
@@ -1,23 +1,50 @@
 // HariFun 138: Playing with AdaFruit's Circuit Playground Arduino board
 // v0 - Read capacitance on port 0.  Works but the threshold need to be adjusted depending on whether we use a wire or touch it directly.
+// v1 - Instead of setting a value threshold, let's try a DIFFERENCE threshold.
 
 #include <Adafruit_CircuitPlayground.h>
 
-int oldCap0 = 0;
-
+int prevCap = 0;
+int prevAve = 0;
+int ave;
+int isOn; 
 void setup() {
   Serial.begin(9600);
   Serial.println("Playing with Circuit Playground");
-  
+
   CircuitPlayground.begin();
+}
+
+//void debug(string label, float value)
+//{
+//  Serial.print(" ");
+//  Serial.print(label);
+//  Serial.print("=");
+//  Serial.print(value);
+//}
+
+void plot(int value)
+{
+  Serial.print(value);
+  Serial.print(" ");
 }
 
 void loop() {
   int cap = CircuitPlayground.readCap(0);
-  if (cap != oldCap0) {
-    Serial.print("Capsense #0: "); Serial.print(cap);
-    if (cap>10) Serial.print(" *");
-     Serial.println();
-    oldCap0 = cap;
+  //plot(prevCap);
+  plot(cap);
+  if (cap != prevCap) {
+    ave = (cap + prevCap) / 2;
+    
+    int change = cap - prevCap;
+    if ( abs(change) > 40)
+    {
+      isOn = (change>0);
+    }
+    prevCap = cap;
   }
+    plot(ave);
+    plot(isOn);
+  Serial.println();
+  delay(500);
 }
